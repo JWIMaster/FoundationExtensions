6a85e00bb47025831c355ae92722469e1e45a28f
JWI <jwi@j-w-i.org>
2025-11-14 19:04:39 +1100

    a

--- Sources/FoundationExtensions/AssociatedObjects.swift
+++ Sources/FoundationExtensions/AssociatedObjects.swift
@@ -5,7 +5,6 @@
 //  Created by James Taylor on 2016-08-31.
 //  Copyright © 2016 James Taylor. All rights reserved.
 //
-//  Updated by J_W_I_
 
 import Foundation
 
@@ -24,71 +23,18 @@ public enum AssociationPolicy: UInt {
 
 public protocol AssociatedObjects: AnyObject {}
 
-// transparent wrappers
-
 public extension AssociatedObjects {
-    
     /// wrapper around `objc_getAssociatedObject`
-    func ao_get(pkey: UnsafeRawPointer) -> Any? {
-        return objc_getAssociatedObject(self, pkey)
+    func getAssociatedObject(key: UnsafeRawPointer) -> AnyObject? {
+        return objc_getAssociatedObject(self, key) as AnyObject
     }
-    
     /// wrapper around `objc_setAssociatedObject`
-    func ao_set(_ value: Any, pkey: UnsafeRawPointer, policy: AssociationPolicy = .retainNonatomic) {
-        objc_setAssociatedObject(self, pkey, value, policy.objc)
+    func setAssociatedObject(_ object: AnyObject, key: UnsafeRawPointer, policy: AssociationPolicy = .retainNonatomic) {
+        objc_setAssociatedObject(self, key, object, policy.objc)
     }
-    
     /// wrapper around 'objc_removeAssociatedObjects'
-    func ao_removeAll() {
+    func removeAssociatedObjects() {
         objc_removeAssociatedObjects(self)
-    }
-}
-
-// string key managed wrappers
-
-fileprivate class MutableBox<T> {
-    
-    private(set) var value: T
-    
-    init(_ value: T) {
-        self.value = value
-    }
-    
-    // accepts a closure that can mutate `value` from within
-    func mutate(f: (inout T) throws -> Void) rethrows {
-        try f(&value)
-    }
-}
-
-fileprivate typealias KeyBox = MutableBox<[String: UnsafePointer<Int8>]>
-fileprivate var keyBoxKey = "_" as Character
-
-fileprivate extension AssociatedObjects {
-    // the box containing the dictionary we'll use to manage string keys
-    var keybox: KeyBox {
-        return ao_get(pkey: &keyBoxKey) as? KeyBox ?? {
-            let keybox = KeyBox([:])
-            ao_set(keybox, pkey: &keyBoxKey)
-            return keybox
-        }()
-    }
-}
-
-public extension AssociatedObjects {
-    
-    /// `String` key managed wrapper around `objc_setAssociatedObject`
-    func ao_set(_ value: Any, key: String) {
-        key.withCString { p in
-            ao_set(value, pkey: p)
-            // store the key
-            keybox.mutate { $0[key] = p }
-        }
-    }
-    
-    /// `String` key managed wrapper around `objc_getAssociatedObject`
-    func ao_get(key: String) -> Any? {
-        guard let p = keybox.value[key] else { return nil }
-        return ao_get(pkey: p)
     }
 }
 
